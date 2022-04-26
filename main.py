56a597384cdc62465d40c9fa3594220aa6dbe003
LegitLemon <daniel.woonings@outlook.com>
2022-04-26 12:06:56 +0200

    Changes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from Simulation import Simulation
 
+
 def main():
     simulation = Simulation()
-    #
     simulation.initClassifier()
     simulation.testClassifier()
-
     # simulation.signalEncoder.plotEncodedSpikesSignals()
     # simulation.run()
+
+
 if __name__ == "__main__":
     main()
+
+
