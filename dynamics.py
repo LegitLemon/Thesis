88f76be14be094941c64b3bc1af990d63c2b7140
LegitLemon <63317579+LegitLemon@users.noreply.github.com>
2022-07-02 10:26:12 +0200

    changes

--- dynamics.py
+++ dynamics.py
@@ -96,3 +96,11 @@ def test_loading(state, tau, a_input, w_connec):
     dxdt = (1/tau)*(decay_input + new_state_input)
 
     return dxdt
+
+# Lorenz attractor
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return [x_dot, y_dot, z_dot]
+
